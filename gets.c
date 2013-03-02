27841e0a84f5b520039841dc834127061c9fd823
NickZhang <looloopig@gmail.com>
2013-03-02 23:24:20 +0800

    Test gets()

---
+++ gets.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main(void) 
+{ 
+    char buff[10]; 
+    memset(buff,0,sizeof(buff)); 
+ 
+    gets(buff); 
+ 
+    printf("\n The buffer entered is [%s]\n",buff); 
+ 
+    return 0; 
+}
